984921f5cbebe3e748864d1e393f3a9467150380
Eduardo Caceres <edu_kueyar@hotmail.com>
2018-03-01 01:37:07 +0100

    Add: *  Write<T>(T) * WriteLine<T>(T) * Write<T>(ICollection<T>) * WriteLine<T>(ICollection<T>) * Clear() * NextLine()

--- FileScriberSolution/FileScriber/Writer.cs
+++ FileScriberSolution/FileScriber/Writer.cs
@@ -11,9 +11,37 @@ namespace FileScriber
 {
     static public class Writer
     {
-        static public void WriteLine<T>(string path, ICollection<T> contentToWrite, char separator = ' ')
+        static public void Clear(string path)
+        {
+            File.Delete(path);
+        }
+
+        static public void NextLine(string path)
+        {
+            try
+            {
+                StreamWriter writer = new StreamWriter(path, true);
+
+                using (writer)
+                {
+                    writer.WriteLine();
+                }
+            }
+            catch (Exception e)
+            {
+                // Possible exceptions:
+                // FileNotFoundException, DirectoryNotFoundException, IOException, ArgumentException
+
+                Print.WriteLine(e.Message);
+                Print.WriteLine("(path: {0}", path);
+                throw e;
+            }
+        }
+
+        static public void Write<T>(string path, ICollection<T> contentToWrite, char separator = ' ', bool isNewLine = false)
         {
             string stringVector = String.Join(separator.ToString(), contentToWrite);
+
             if ((separator == ',' || separator == '.')
                 && contentToWrite.Count > 0
                 && contentToWrite.First().GetType() == typeof(double))
@@ -21,18 +49,41 @@ namespace FileScriber
                 Print.WriteLine("Written text might no be parseable due to coincidence among element separators and number decimal separator ");
             }
 
-            WriteLine(path, stringVector);
+            ScribeString(path, stringVector, isNewLine);
         }
 
-        static public void WriteLine(string path, string textInFile)
+        static public void WriteLine<T>(string path, ICollection<T> contentToWrite, char separator = ' ')
+        {
+            Write(path, contentToWrite, separator, true);
+        }
+
+        static public void Write<T>(string path, T element, char separator = ' ', bool isNewLine = false)
+        {
+            string stringSeparator = isNewLine == true
+                ? string.Empty
+                : separator.ToString();
+
+            ScribeString(path, Convert.ToString(element) + stringSeparator, isNewLine);
+        }
+
+        static public void WriteLine<T>(string path, T element, char separator = ' ')
+        {
+            Write<T>(path, element, separator, true);
+        }
+
+
+        static private void ScribeString(string path, string textInFile, bool isWriteLine = false)
         {
             try
             {
-                StreamWriter writer = new StreamWriter(path);
+                StreamWriter writer = new StreamWriter(path, true);
 
                 using (writer)
                 {
-                    writer.WriteLine(textInFile);
+                    if (isWriteLine)
+                        writer.WriteLine(textInFile);
+                    else
+                        writer.Write(textInFile);
                 }
             }
             catch (Exception e)
--- FileScriberSolution/FileScriberTest/BasicTest.cs
+++ FileScriberSolution/FileScriberTest/BasicTest.cs
@@ -1,20 +1,29 @@
-
 using System;
-using Xunit;
-
-using FileScriber;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
+using Xunit;
+
+using FileScriber;
+using FileParser;
+using System.Globalization;
 
 namespace FileScriberTest
 {
     public class FileWriterTest
     {
-        [Fact]
-        public void WriteLineString()
+        public FileWriterTest()
         {
-            string path = "WriteLineString.txt";
+            var cultureInfo = new CultureInfo("en-US");
+            CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+        }
+
+        [Fact]
+        public void WriteElementString()
+        {
+            string path = "WriteElementString.txt";
+            Writer.Clear(path);
+
             string testString = " This is a  1  2 test! ";
             string textInFile = string.Empty;
 
@@ -35,9 +44,55 @@ namespace FileScriberTest
         }
 
         [Fact]
-        public void WriteLineDoubleVector()
+        public void WriteMultipleElements()
         {
-            string path = "WriteLineDoubleVector.txt";
+            string path = "WriteMultipleElements.txt";
+            Writer.Clear(path);
+
+            bool _bool = false;
+            Writer.Write(path, _bool);
+
+            int _int = 1;
+            Writer.Write(path, _int);
+
+            double _double = 3.14159265;
+            Writer.Write(path, _double);
+
+            string _string = " Vishy  ";
+            Writer.Write(path, _string);
+
+            DateTime _dateTime = new DateTime(1999, 12, 31, 23, 59, 59);
+            Writer.WriteLine(path, _dateTime);
+
+            Writer.NextLine(path);      // Extra line
+
+            Writer.WriteLine(path, _bool);
+            Writer.WriteLine(path, _int);
+            Writer.WriteLine(path, _double);
+            Writer.WriteLine(path, _string);
+
+            IParsedFile parsedFile = new ParsedFile(path);
+            IParsedLine firstLine = parsedFile.NextLine();
+
+            Assert.Equal(_bool, firstLine.NextElement<bool>());
+            Assert.Equal(_int, firstLine.NextElement<int>());
+            Assert.Equal(_double, firstLine.NextElement<double>());
+            Assert.Equal(_string.Trim(), firstLine.NextElement<string>());
+
+            Assert.Equal(_bool, parsedFile.NextLine().NextElement<bool>());
+            Assert.Equal(_int, parsedFile.NextLine().NextElement<int>());
+            Assert.Equal(_double, parsedFile.NextLine().NextElement<double>());
+            Assert.Equal(_string.Trim(), parsedFile.NextLine().NextElement<string>());
+
+            Assert.True(parsedFile.Empty);
+        }
+
+        [Fact]
+        public void WriteListDouble()
+        {
+            string path = "WriteListDouble.txt";
+            Writer.Clear(path);
+
             ICollection<double> vectorToWrite = new List<double>() { 0.0, 1.1, 2.22, 3.33 };
             string textInFile = string.Empty;
             char separator = '%';
@@ -56,6 +111,35 @@ namespace FileScriberTest
             }
 
             List<double> vectorInFile = textInFile.Split(separator).Select(str => double.Parse(str.Trim())).ToList();
+
+            Assert.Equal(vectorToWrite, vectorInFile);
+        }
+
+        [Fact]
+        public void WriteListString()
+        {
+            string path = "WriteListString.txt";
+            Writer.Clear(path);
+
+            ICollection<string> vectorToWrite = new List<string>() { "string1", "string2", "string3", "string4" };
+            string textInFile = string.Empty;
+            char separator = '$';
+
+            Writer.WriteLine(path, vectorToWrite, separator);
+
+            IParsedFile file = new ParsedFile(path, new char[] { separator });
+            while (!file.Empty)
+            {
+                IParsedLine line = file.NextLine();
+                while (!line.Empty)
+                {
+                    textInFile += line.NextElement<string>();
+                    if (!line.Empty)
+                        textInFile += "@";
+                }
+            }
+
+            List<string> vectorInFile = textInFile.Split('@').ToList();
 
             Assert.Equal(vectorToWrite, vectorInFile);
         }
