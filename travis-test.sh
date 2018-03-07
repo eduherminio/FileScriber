54e13d7314d4be8e77a716532935358afb28137b
Eduardo Caceres <edu_kueyar@hotmail.com>
2018-03-07 21:58:42 +0100

    :construction_worker: Fix Travis CI build and tests

--- travis-test.sh
+++ travis-test.sh
@@ -3,7 +3,7 @@
 #exit if any command fails 
 set -e 
 
-dotnet test ./FileScriberSolution/FileScriberTest
+dotnet test ./FileScriberSolution/FileScriberTest --framework=netcoreapp2.0
 
 revision=${TRAVIS_JOB_ID:=1}
 revision=$(printf "%04d" $revision)
